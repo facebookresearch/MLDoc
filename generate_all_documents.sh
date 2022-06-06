aa725fd9c450414afc1c1d15a1937bbb12062a92
Rajarshee Mitra <rajarsheem@gmail.com>
2022-06-06 14:47:49 +0530

    correct indentation

--- generate_all_documents.sh
+++ generate_all_documents.sh
@@ -7,7 +7,8 @@ for filename in mldoc-indices/*; do
         echo $lang
         mkdir -p /vc_data/users/ramitra/data/MLDoc/$lang/
         if [[ "$lang" == *"english"* ]]; then
-                python generate_documents.py --indices-file $filename --output-filename $OUT_DIR/$lang/$n --rcv-dir $RCV_DIR/rcv1/        else
+                python generate_documents.py --indices-file $filename --output-filename $OUT_DIR/$lang/$n --rcv-dir $RCV_DIR/rcv1/        
+        else
                 python generate_documents.py --indices-file $filename --output-filename $OUT_DIR/$lang/$n --rcv-dir $RCV_DIR/RCV2_Multilingual_Corpus/$lang/
         fi
 
